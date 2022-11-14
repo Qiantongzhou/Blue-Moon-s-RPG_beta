01e79674dc4456add264f4a1bd77703951128c7f
Yuchen Huang <YuchenHuang97@Gmail.com>
2022-11-13 20:28:30 -0500

    merge commit main

--- Assets/characters/Mechnics/DamageDealer.cs
+++ Assets/characters/Mechnics/DamageDealer.cs
@@ -3,22 +3,30 @@ public class DamageDealer : MonoBehaviour
 {
     [SerializeField]
     private LayerMask EnemyMask;
+    [SerializeField]
+    private float DamageAmount;
 
-    private Collider myCollider;
+    private HitAudio hitAudio;
 
     private void Awake()
     {
-        myCollider = GetComponent<Collider>();
+        hitAudio = GetComponent<HitAudio>();
     }
     private void OnTriggerEnter(Collider other)
     {
         if (EnemyMask == (EnemyMask | (1 << other.gameObject.layer)))
         {
-            DealDamage();
+            hitAudio.Hit();
+            if (other.GetComponent<Health>() != null)
+            {
+                Vector3 direction = transform.position - other.gameObject.transform.position;
+                Vector3 direction2D = new Vector3(direction.x, 0f, direction.z);
+                DealDamage(other.GetComponent<Health>(), Vector3.Normalize(direction2D));
+            }
         }
     }
-    private void DealDamage()
+    private void DealDamage(Health health, Vector3 direction)
     {
-        Debug.Log("Hit");
+        health.TakeDamage(DamageAmount, direction);
     }
 }
